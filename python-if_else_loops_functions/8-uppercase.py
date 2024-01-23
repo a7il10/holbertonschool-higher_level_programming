f6ea04cb304327ef31f1e6c21ef78edcec33b50f
a7il10 <atilio.sulovari199@gmail.com>
2024-01-23 12:53:40 +0100

    add code ex 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return ''.join([chr(ord(char) - 32) for char in str if ord(char) >= 65])
+	for char in str:
+		if ord(char) >= 97 and ord(char) <= 122:
+			char = chr(ord(char) - 32)
+		print("{}".format(char), end="")
+	print()
+uppercase("best")
+uppercase("Best School 98 Battery street")
