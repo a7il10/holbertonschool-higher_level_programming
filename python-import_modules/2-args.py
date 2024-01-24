9da350d7146000ed91fb0155f0bd07f8fa5e42a8
a7il10 <atilio.sulovari199@gmail.com>
2024-01-24 12:37:58 +0000

    update ex 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
         print("{} argument:".format(len(argv) - 1))
         print("1: {}".format(argv[1]))
     else:
-        print("{} argument:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
