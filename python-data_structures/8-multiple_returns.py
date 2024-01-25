2af8f4c6d3f5b78644649034542481fa72449c17
a7il10 <atilio.sulovari199@gmail.com>
2024-01-25 22:17:46 +0000

    fix code ex 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,5 +4,6 @@ def multiple_returns(sentence):
         first_char = None
     else:
         first_char = sentence[0]
-        new_tuple = (len(sentence), first_char)
-        return new_tuple
+
+    new_tuple = (len(sentence), first_char)
+    return new_tuple
