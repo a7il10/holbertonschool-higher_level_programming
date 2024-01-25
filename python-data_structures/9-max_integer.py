5bf2c33ea21e0f0cf1d81f6902cc1ecd5edb57c1
a7il10 <atilio.sulovari199@gmail.com>
2024-01-25 22:08:52 +0000

    ex 9 update

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    largest = my_list[0]
-    for n in my_list:
-        if len(my_list) == 0:
-            return None
-        elif n > largest:
-            largest = n
-            return largest
+    if len(my_list) == 0:
+        return None
+    else:
+        largest = my_list[0]
+        for n in my_list:
+            if largest < n:
+                largest = n
+        return largest
