1de2b00585e495589a27e72ec43120e52c30787b
a7il10 <atilio.sulovari199@gmail.com>
2024-01-31 13:11:56 +0000

    ex 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,11 +20,11 @@ class Square:
 
     @size.setter
     def size(self, value):
+        self.__size == value
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if self.__size < 0:
             raise ValueError('size must be >= 0')
-        self.__size == value
 
     def area(self):
         return self.__size * self.__size
