75f7ec817a1962b1e14bb7844b9f776fb53babba
a7il10 <atilio.sulovari199@gmail.com>
2024-01-31 12:56:33 +0000

    ex 4, 5

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Write a class Square that defines a square
+'''
+
+
+class Square:
+
+    '''
+    Private instance attribute: size
+    '''
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return self.__size ** 2
---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+Write a class Square that defines a square
+'''
+
+
+class Square:
+
+    '''
+    Private instance attribute: size
+    '''
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size == value
+
+    def area(self):
+        return self.__size ** 2
