15289e7c722bce93c0df5d094a31a3da76dc5855
a7il10 <atilio.sulovari199@gmail.com>
2024-01-31 13:25:47 +0000

    ex 6

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,7 +6,6 @@ Write a class Square that defines a square
 
 
 class Square:
-
     '''
     Private instance attribute: size
     '''
@@ -20,11 +19,11 @@ class Square:
 
     @size.setter
     def size(self, value):
+        self.__size = value
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if value < 0:
+        if self.__size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = value
 
     def area(self):
         return self.__size * self.__size
