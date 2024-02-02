077c807bdf8dc0bd00c969b41c076f365f300251
a7il10 <atilio.sulovari199@gmail.com>
2024-02-02 12:04:54 +0000

    ex 2 udate code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-""" first and last name"""
+
+'''
+
+    Module to test doctest module with a simple function which
+    concatenates two strings together
+'''
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    first and last name
-    """
 
-    if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
-    if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    print(f"{first_name} {last_name}")
+    '''
+
+    Function who takes two string as arguments
+    and prints them in a sentence
+    '''
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
