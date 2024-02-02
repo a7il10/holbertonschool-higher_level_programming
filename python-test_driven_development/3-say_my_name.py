eef337bb9a7889c31cda9c9d807a09b5fa422677
a7il10 <atilio.sulovari199@gmail.com>
2024-02-02 12:03:47 +0000

    ex 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,10 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    first and last name
+    """
+
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
