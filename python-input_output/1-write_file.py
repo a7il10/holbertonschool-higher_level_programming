16ab96e64b6e0edb288756c1d518d5f4fa87df1a
a7il10 <atilio.sulovari199@gmail.com>
2024-02-08 12:34:50 +0000

    ex 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding='utf-8') as file:
-        contents = file.read()
-    return len(contents)
+        text = file.read()
+    return len(text)
