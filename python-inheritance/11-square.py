bc3ebbca0d5fdc1ff906fe9263487995434e7ea3
a7il10 <atilio.sulovari199@gmail.com>
2024-02-08 10:43:06 +0000

    fix ex 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size):
+        Rectangle.integer_validator(self, "size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
