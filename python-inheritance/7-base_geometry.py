901049a7ec5246ac159a3fb76609093877c56d6b
a7il10 <atilio.sulovari199@gmail.com>
2024-02-08 09:59:26 +0000

    fix ex 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class BaseGeometry:
+    """Defines an empty class"""
+    def area(self):
+        """Improve geometry"""
+        raise Exception('area() is not implemented')
+    
+    def integer_validator(self, name, value):
+        """Public instance method: def integer_validator(self, name, value):
+            that validates value"""
+        if not isinstance(value, int):
+            TypeError(f"{name} must be an integer")
+        if value <= 0:
+            ValueError(f"{name} must be greater than 0")
