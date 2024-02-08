9dc8616f4ccd02bebbbc8270f469b4d9f73f8515
a7il10 <atilio.sulovari199@gmail.com>
2024-02-08 09:30:52 +0000

    add comments

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,4 +10,7 @@ class MyList(list):
     Module that inherits from list
     '''
     def print_sorted(self):
+        '''
+        Module that inherits from list
+        '''
         print(sorted(self))
