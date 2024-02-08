9f76ed7e9f76ea84bf4347abc0196660c1631388
a7il10 <atilio.sulovari199@gmail.com>
2024-02-08 12:32:36 +0000

    ex 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def write_file(filename="", text=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        contents = file.read()
+    return len(contents)
