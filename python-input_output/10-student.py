5e582ae0770ccd442f6f45049da3605fc044fc35
a7il10 <atilio.sulovari199@gmail.com>
2024-02-09 12:47:57 +0000

    ex 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,11 +2,8 @@
 """class Student that defines a student by: (based on 9-student.py)"""
 
 
-#!/usr/bin/python3
-"""class Student that defines a student by"""
-
-
 class Student:
+    """class Student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
