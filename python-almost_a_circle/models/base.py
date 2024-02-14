53bf9f680d9ab36006cf5ebca2982d69cdb036f8
a7il10 <atilio.sulovari199@gmail.com>
2024-02-14 10:20:32 +0000

    ex 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Write the first class Base"""
+import json
 
 
 class Base:
@@ -19,4 +20,4 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-            return json.dump(list_dictionaries)
+            return json.dumps(list_dictionaries)
