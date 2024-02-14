ff77010e4b06542e75741335cbb09b454773f932
a7il10 <atilio.sulovari199@gmail.com>
2024-02-14 09:32:15 +0000

    ex 10 update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,9 +7,8 @@ class Square(Rectangle):
     """class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """class Square that inherits from Rectangle"""
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """str"""
-        return (f"[Square] ({id}) {x}/{y} - "
-                f"{size}")
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
